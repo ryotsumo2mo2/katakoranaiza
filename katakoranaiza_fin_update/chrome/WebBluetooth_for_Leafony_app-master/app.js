f2e81454f516e982dd59680e33dc56fbebe7e7e0
ryotsumo2mo2 <136458328+ryotsumo2mo2@users.noreply.github.com>
2023-06-25 11:09:47 +0900

    Update app.js

--- katakoranaiza_fin_update/chrome/WebBluetooth_for_Leafony_app-master/app.js
+++ katakoranaiza_fin_update/chrome/WebBluetooth_for_Leafony_app-master/app.js
@@ -398,6 +398,7 @@ const main = (fr0) =>{
 	//以上追加点------------------
 
 	//グラフ非表示は以下をコメントアウト
+	/*
     const myChart = new Chart(ctx, {
         type: 'scatter',
         data: {
@@ -432,6 +433,7 @@ const main = (fr0) =>{
         // options: {}, ...
       });
       console.log(myChart);
+      */
 	  
 }
 
